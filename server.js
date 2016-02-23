6cf450bd32f8684738406c5c8c34793286a2515a
ckm100 <kufogbemawuli1@gmail.com>
2016-02-23 14:54:25 +0000

    Updated

--- server.js
+++ server.js
@@ -4,7 +4,8 @@ var http = require("http"),
     ip = require("ip"),
     app = express(),
     lan,
-    soft;
+    soft,
+    realip;
 
 app.set('port', (process.env.PORT || 9000))
 
@@ -12,13 +13,15 @@ app.use(express.static(path.join(__dirname, "public")));
 
 app.get("/whoami", function (req, res) {
 
+    realip = req.headers['x-forwarded-for'].split(',');
+
     lan = req.headers["accept-language"].split(",")[0];
 
     soft = req.headers['user-agent'].match(/\(.*\)/).join(",").split(")")[0];
 
     res.json({
 
-        "ipaddress": req.connection.remoteAddress,
+        "ipaddress": realip[realip.length - 1],
         "language": lan,
         "software": soft.substring(1, soft.length)
     });
